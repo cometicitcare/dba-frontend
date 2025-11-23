7151f7ab4c35a1e28d02b59ae4462284816d8a4c
neranjanpriyashantha <neranjanapriyashantha1997@gmail.com>
2025-11-23 17:48:03 +0530

    Update to create a manage page for  the high bhikkhu

--- src/services/bhikku.ts
+++ src/services/bhikku.ts
@@ -1,12 +1,27 @@
 import BackendClient from './backendClient';
 import { baseURL } from "../utils/config";
 export const _manageBhikku = (body: any) => BackendClient.post(`${baseURL}/bhikkus/manage`, body);
-export const _manageHighBhikku = (body: any) => BackendClient.post(`${baseURL}/bhikkus-high/manage`, body);
 export const _manageSilmatha = (body: any) => BackendClient.post(`${baseURL}/silmatha-regist/manage`, body);
 export const _approveBhikkhu = (br_regn: any) => BackendClient.post(`${baseURL}/bhikkus/${br_regn}/approve`);
 export const _rejectBhikkhu = (br_regn: any, body: any) => BackendClient.post(`${baseURL}/bhikkus/${br_regn}/reject`, body);
 export const _markPrintedBhikkhu = (br_regn: any) => BackendClient.post(`${baseURL}/bhikkus/${br_regn}/mark-printed`);
 export const _uploadScannedDocument = (br_regn: any, file: File) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  return BackendClient.post(
+    `${baseURL}/bhikkus/${br_regn}/upload-scanned-document`,
+    formData,
+    { headers: { "Content-Type": "multipart/form-data" } }
+  );
+};
+
+
+// High Bhikkhu
+export const _manageHighBhikku = (body: any) => BackendClient.post(`${baseURL}/bhikkus-high/manage`, body);
+export const _approveHighBhikkhu = (br_regn: any) => BackendClient.post(`${baseURL}/bhikkus/${br_regn}/approve`);
+export const _rejectHighBhikkhu = (br_regn: any, body: any) => BackendClient.post(`${baseURL}/bhikkus/${br_regn}/reject`, body);
+export const _markPrintedHighBhikkhu = (br_regn: any) => BackendClient.post(`${baseURL}/bhikkus/${br_regn}/mark-printed`);
+export const _uploadScannedHighDocument = (br_regn: any, file: File) => {
   const formData = new FormData();
   formData.append("file", file);
   return BackendClient.post(
