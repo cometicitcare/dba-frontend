90b230f28c92936c0bf3610f19174b92bc2e8756
Yasiru-Tech <yasirudeshan.tech@gmail.com>
2025-12-10 23:28:21 +0530

    fix: minor fix

--- src/services/arama.ts
+++ src/services/arama.ts
@@ -1,3 +1,4 @@
 import BackendClient from './backendClient';
 import { baseURL } from "../utils/config";
 export const _aramaManage = (body: any) => BackendClient.post(`${baseURL}/silmatha-regist/arama-list`, body);
+export const _manageArama = (body: any) => BackendClient.post(`${baseURL}/arama-data/manage`, body);
