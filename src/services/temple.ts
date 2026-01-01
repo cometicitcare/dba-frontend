09525da925d59ddcbec94990c05747b2a64ea4df
neranjanpriyashantha <neranjanapriyashantha1997@gmail.com>
2026-01-01 15:05:47 +0530

    update the sidebar for DIVITIONAL_SEC_MANAGEMENT_DEPARTMENT

--- src/services/temple.ts
+++ src/services/temple.ts
@@ -2,3 +2,4 @@ import BackendClient from './backendClient';
 import { baseURL } from "../utils/config";
 export const _manageTemple = (body: any) => BackendClient.post(`${baseURL}/vihara-data/manage`, body);
 export const _listTemple = (body: any) => BackendClient.post(`${baseURL}/bhikkus/vihara-list`, body);
+export const _manageTempTemple = (body: any) => BackendClient.post(`${baseURL}/temporary-vihara/manage`, body);
